3bcb2638b78eaeb1a3c2e0b73a58a9747378a6cb
Alexandre Konioukhov <alexan1@gmail.com>
2019-09-14 20:54:57 -0400

    form

--- Medfar.Interview.Web/Controllers/ExamController.cs
+++ Medfar.Interview.Web/Controllers/ExamController.cs
@@ -12,5 +12,11 @@ namespace Medfar.Interview.Web.Controllers
         {
             return View();
         }
+        
+        public ActionResult AddUser(User)
+        {
+            return View();
+        }
+
     }
 }
